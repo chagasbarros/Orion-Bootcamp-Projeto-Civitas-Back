d09d9393c45c31b12fd4c4847808ae214d38d27a
Murilo Salvador <murilo.salvador95@gmail.com>
2024-10-16 02:56:43 -0300

    remove: remocao de rotas inutilizadas

--- src/routes.ts
+++ src/routes.ts
@@ -9,7 +9,4 @@ router.get('/', new HomeController().hello);
 // Auth
 router.post('/login', new AuthController().login);
 
-// Middleware
-router.use('/protected', new AuthController().middleware);
-
 export default router;
