49f569c2786bd5a2f2b61c915d40f627d0fe4c62
Murilo Salvador <murilo.salvador95@gmail.com>
2024-10-16 03:03:48 -0300

    remove: funcoes inutilizadas

--- src/repositories/TokenRepository.ts
+++ src/repositories/TokenRepository.ts
@@ -6,22 +6,4 @@ export class TokenRepository extends Repository<Token> {
   constructor() {
     super(Token, MysqlDataSource.manager);
   }
-
-  /**
-   * Cria ou atualiza um token, caso exista um vencido, do usuário
-   * @param token Token criado com JWT
-   * @param expiresAt Data da expiração do token
-   * @param userId Id do usuário
-   */
-  async saveToken(
-    token: string,
-    expiresAt: Date,
-    userId: number
-  ): Promise<void> {
-    await this.save({
-      token: token,
-      expiresAt: expiresAt,
-      userId: userId
-    });
-  }
 }
