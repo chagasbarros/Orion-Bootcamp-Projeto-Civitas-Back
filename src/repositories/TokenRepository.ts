7a719fea8b080802d52d44b12630c18cd31dfd46
Murilo Salvador <murilo.salvador95@gmail.com>
2024-10-16 02:55:07 -0300

    remove: remoção de funções inutilizadas

--- src/repositories/TokenRepository.ts
+++ src/repositories/TokenRepository.ts
@@ -1,7 +1,6 @@
 import { Repository } from 'typeorm';
 import { MysqlDataSource } from '../config/database';
 import { Token } from '../entity/Token';
-import { Role } from '../entity/Role';
 
 export class TokenRepository extends Repository<Token> {
   constructor() {
@@ -16,38 +15,13 @@ export class TokenRepository extends Repository<Token> {
    */
   async saveToken(
     token: string,
-    role: Role,
     expiresAt: Date,
     userId: number
   ): Promise<void> {
     await this.save({
       token: token,
-      role: role,
       expiresAt: expiresAt,
       userId: userId
     });
-  }
-
-  /**
-   * Retorna Token de acordo com Id do usuário e permissão pedida
-   * @param userId Id do usuário
-   * @param role Nível de permissão pedido
-   * @returns Token do usuário com nível de permissão pedido
-   */
-  async findTokenByUserIdAndRole(
-    userId: number,
-    roleStr: string
-  ): Promise<Token | undefined> {
-    return this.findOne({
-      where: { userId: userId, role: { authType: roleStr } }
-    });
-  }
-
-  /**
-   * Remove token
-   * @param token Token a ser removido
-   */
-  async removeToken(token: Token) {
-    this.remove(token);
   }
 }
